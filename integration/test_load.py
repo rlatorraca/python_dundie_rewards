37bad7efcd43f90ad07a00fafbe3133982a05e01
Rodrigo Latorraca de S Pires <rlatorraca@gmail.com>
2024-06-29 10:58:36 -0300

    Error (#18)
    * added: add an error on unit test
    
    * modified: actions/setup-python@v2 to @v4
    
    * Added: junit reports
    
    * ammend: typo on main.yaml
    
    * ammend
    
    * ammend
    
    * ammend
    
    * ammend
    
    * ammend
    
    * fixed: error unit tests
    
    * fixed: error unit tests
    
    * fixed: error unit tests
    
    * Added: negative integration test to wrong_command for load

--- integration/test_load.py
+++ integration/test_load.py
@@ -1,11 +1,23 @@
 import pytest
-from subprocess import check_output # For Running Linux commands
+from subprocess import check_output, CalledProcessError # For Running Linux commands
 from .constants import PEOPLE_FILE
 
 @pytest.mark.integration
 @pytest.mark.medium
-def test_load():
+def test_load_positive_call_load_command():
     """Test command load into terminal"""
     output = check_output(['dundie', 'load', PEOPLE_FILE]).decode('utf-8').split('\n')
 
     assert len(output) == 101
+
+@pytest.mark.integration
+@pytest.mark.medium
+@pytest.mark.parametrize('wrong_command', [
+    'loady', 'open', 'read', 'fetch', 'retrieve',
+    'access', 'ingest', 'initialize', 'unpack'])
+def test_load_negative_call_load_command_with_wrong_parameters(wrong_command):
+    """Test command load into terminal"""
+    with pytest.raises(CalledProcessError) as error:
+        check_output(['dundie', wrong_command, PEOPLE_FILE]).decode('utf-8').split('\n')
+
+    assert "status 2" in str(error.getrepr())

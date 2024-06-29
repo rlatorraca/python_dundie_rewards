be2387f3fd37bad44348b98a3d19d69711659d8b
Rodrigo Latorraca <rlatorraca@gmail.com>
2024-06-29 00:40:14 -0300

    added: add an error on unit test

--- tests/test_load.py
+++ tests/test_load.py
@@ -4,7 +4,12 @@ from .constants import PEOPLE_FILE
 
 @pytest.mark.unit
 @pytest.mark.high
-def test_load():
+def test_load_positive_total_100_people():
     """Test Dundie/core.load() function"""
-    assert len(load(PEOPLE_FILE)) == 100
+    assert len(load(PEOPLE_FILE)) == 101
+
+@pytest.mark.unit
+@pytest.mark.high
+def test_load_positive_first_name_like_Eve():
+    """Test Dundie/core.load() function"""
     assert load(PEOPLE_FILE)[0][0:3] == 'Eve'

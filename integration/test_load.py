8f8249dd3662e05e525c910e29bb46fe17a670d2
Rodrigo Latorraca <rlatorraca@gmail.com>
2024-07-01 00:19:49 -0300

    added: Formated click + rich + rich_click + table

--- integration/test_load.py
+++ integration/test_load.py
@@ -13,14 +13,13 @@ cmd = CliRunner()
 def test_load_positive_call_load_command():
     """Test command load into terminal"""
     out = cmd.invoke(load, PEOPLE_FILE)
-    assert 'Dundie Mifflin Reward - Associates' in out.output
+    assert "Dundie Mifflin Reward - Associates" in out.output
 
 
 @pytest.mark.integration
 @pytest.mark.medium
 @pytest.mark.parametrize(
-    "wrong_command",
-    ["loady", "open", "read", "fetch", "retrieve", "access", "ingest", "initialize", "unpack"]
+    "wrong_command", ["loady", "open", "read", "fetch", "retrieve", "access", "ingest", "initialize", "unpack"]
 )
 def test_load_negative_call_load_command_with_wrong_parameters(wrong_command):
     """Test command load into terminal"""

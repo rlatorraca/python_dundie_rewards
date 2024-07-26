df7ce208e1a1710d89b44c9e45386198cb944ef3
Rodrigo Latorraca de S Pires <rlatorraca@gmail.com>
2024-07-25 22:39:36 -0300

    Update exchange.py

--- dundie/utils/exchange.py
+++ dundie/utils/exchange.py
@@ -5,24 +5,25 @@ from dundie.settings import API_BASE_URL
 from pydantic import BaseModel, Field
 
 
-class usd_rate(BaseModel):
+class UsdRate(BaseModel):
     code: str = Field(default="USD")
     codein: str = Field(default="USD")
     name: str = Field(default="Dolar/Dolar")
     value: Decimal = Field(alias="high")
 
 
-def get_rates(currencies: List[str]) -> Dict[str, usd_rate]:
+def get_rates(currencies: List[str]) -> Dict[str, UsdRate]:
     """Get the current rates of dolar in each currency"""
     return_data = {}
     for currency in currencies:
         if currency == "USD":
-            return_data[currency] = usd_rate(high=1)
+            return_data[currency] = UsdRate(high=Decimal('1.0'))
         else:
             response = httpx.get(API_BASE_URL.format(currency=currency))
             if response.status_code == 200:
                 data = response.json()["USD" + currency]
-                return_data[currency] = usd_rate(**data)
+                data["high"] = Decimal(data["high"])
+                return_data[currency] = UsdRate(**data)
             else:
-                return_data[currency] = usd_rate(name="API/ERROR", high=0)
+                return_data[currency] = UsdRate(name="API/ERROR", high=Decimal('0.0'))
     return return_data

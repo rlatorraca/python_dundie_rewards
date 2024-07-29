9d8f79d91e5a3010ee20d1d11c73bbafb55c6aaa
Rodrigo Latorraca <rlatorraca@gmail.com>
2024-07-28 23:22:48 -0300

    amend: coverage and new tests for testing

--- dundie/utils/auth.py
+++ dundie/utils/auth.py
@@ -1,11 +1,9 @@
-
-
 from functools import wraps
-import os
 
 
 def requires_authentication(f):
     @wraps(f)
     def decorator(self, *args, **kwargs):
-        email = os.getenv('DUNDIE_EMAIL')
-        password = os.getenv('DUNDIE_PASSWORD')
+        """Decorator for authenticating"""
+        # email = os.getenv('DUNDIE_EMAIL')
+        # password = os.getenv('DUNDIE_PASSWORD')
--- tests/test_load.py
+++ tests/test_load.py
@@ -11,12 +11,14 @@ def test_load_positive_total_100_people(request):
     """Test Dundie/core.load() function"""
     assert len(load(PEOPLE_FILE)) == 100
 
+
 @pytest.mark.unit
 @pytest.mark.high
 def test_load_negative_file_not_found(request):
     """Test Dundie/core.load() function"""
     with pytest.raises(FileNotFoundError):
         assert load("assets/invalid_file.csv")
+
 
 @pytest.mark.unit
 @pytest.mark.high
